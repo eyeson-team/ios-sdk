cc18ddcbcb2fab3aca9edecff896e3ea2cf676a0
iOS Developer <michael.maier@eyeson.com>
2022-07-21 15:05:07 +0200

    added Swift Package

---
+++ Eyeson.xcframework/ios-arm64/Eyeson.framework/Headers/Eyeson.h
@@ -0,0 +1,14 @@
+//
+//  eyesonSDK.h
+//  eyesonSDK
+//
+//  Created by Michael Maier on 13.12.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Eyeson.
+FOUNDATION_EXPORT double EyesonVersionNumber;
+
+//! Project version string for Eyeson.
+FOUNDATION_EXPORT const unsigned char EyesonVersionString[];
