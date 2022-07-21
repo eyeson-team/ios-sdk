000470638d19725402d93f4820db738f7387fabb
iOS Developer <michael.maier@eyeson.com>
2022-07-21 16:28:26 +0200

    fixes manifest

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: https://github.com/SwiftyJSON/SwiftyJSON.git),
-        .package(url: https://github.com/daltoniam/Starscream, from: "3.1.0"),
-        .package(url: https://github.com/alexpiezo/WebRTC.git),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git"),
+        .package(url: "https://github.com/daltoniam/Starscream", from: "3.1.0"),
+        .package(url: "https://github.com/alexpiezo/WebRTC.git"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
