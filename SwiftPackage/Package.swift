c29485e446d0e363e230d5dcbcf0eadf385f3714
iOS Developer <michael.maier@eyeson.com>
2022-07-21 15:22:04 +0200

    Release Swift Package 1.0.2

--- SwiftPackage/Package.swift
+++ SwiftPackage/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Eyeson",
-            path: "../Eyeson.xcframework"
+            path: "Sources/Eyeson.xcframework"
         )
     ]
 )
