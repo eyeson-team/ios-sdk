8755cb02decca98ca7ef6b0737af47007eed5ae9
iOS Developer <michael.maier@eyeson.com>
2022-07-22 09:46:21 +0200

    added package dependencies

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .branch("master")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", branch: "master"),
         .package(url: "https://github.com/daltoniam/Starscream", from: "3.1.0"),
-        .package(url: "https://github.com/livekit/WebRTC-swift", .branch("master"))
+        .package(url: "https://github.com/livekit/WebRTC-swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
