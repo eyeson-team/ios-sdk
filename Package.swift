eb8fedc0a4aa7f2aed1373fd8e5fbc9604b1e842
iOS Developer <michael.maier@eyeson.com>
2022-08-17 12:59:27 +0200

    added static linked libraries

--- Package.swift
+++ Package.swift
@@ -9,20 +9,11 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EyesonSdk",
             targets: ["EyesonSdk"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", branch: "master"),
-        .package(url: "https://github.com/daltoniam/Starscream", from: "3.1.0"),
-        .package(url: "https://github.com/livekit/WebRTC-swift", branch: "master")
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "EyesonSdk",
             path: "Sources/EyesonSdk.xcframework"
