943615c617b738e67314a8654750b0eb33f5fd21
iOS Developer <michael.maier@eyeson.com>
2022-08-18 16:04:25 +0200

    updates dependency list

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     products: [
         .library(
             name: "EyesonSdk",
-            targets: ["EyesonSdk", "Starscream", "SwiftyJSON"])
+            targets: ["EyesonSdk", "Starscream", "SwiftyJSON", "WebRTC"])
     ],
     targets: [
         .binaryTarget(
